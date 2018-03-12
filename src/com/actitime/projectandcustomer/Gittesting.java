307cde228a6038c6914dafc5c4e5569616bfa64f
Satyajit Bihari <biharisatyajit@gmail.com>
2018-03-12 12:48:11 +0530

    first commit

---
+++ src/com/actitime/projectandcustomer/Gittesting.java
@@ -0,0 +1,12 @@
+package com.actitime.projectandcustomer;
+
+public class Gittesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hi");
+		
+	}
+
+}
